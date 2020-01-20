0ae39a86fd4ba8e4bca9450ea9cfb1f106a82585
Jacek Sobiecki <jsobiecki01@gmail.com>
2020-01-20 21:53:24 +0100

    v0.3

--- routes/saveFormReq.js
+++ routes/saveFormReq.js
@@ -9,16 +9,17 @@ router.post('/', function(req, res, next) {
     const reqType = req.body.reqType
 
     console.log(form)
-    console.log(reqType)
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+reqType)
 
     var MongoClient = require('mongodb').MongoClient;
     var url = "mongodb://localhost:27017/";
-
-    console.log(">>>>>>>>>>>>>>"+form.currentState.name)
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         const dbo = db.db("mydb");
         if(reqType==="save"){
+            var ObjectID = require('mongodb').ObjectID
+            var x = ObjectID()
+            form["_id"]=x.toString()
             dbo.collection("forms").insertOne(form, function(err, res) {
                 if (err) throw err;
                 console.log("1 document inserted");
@@ -26,18 +27,18 @@ router.post('/', function(req, res, next) {
             });
         } else if(reqType==="update"){
             console.log("##########"+form._id)
-            dbo.collection("forms").drop({_id: form._id}, function(err, res) {
+            dbo.collection("forms").remove({_id: form._id}, function(err, res) {
                 if (err) throw err;
-                console.log("1 document inserted");
+                console.log("update: document deleted");
                 db.close();
             });
             dbo.collection("forms").insertOne(form, function(err, res) {
                 if (err) throw err;
-                console.log("1 document inserted");
+                console.log("update: document inserted");
                 db.close();
             });
         } else if(reqType==="delete"){
-            dbo.collection("forms").drop({_id: form._id}, function(err, res) {
+            dbo.collection("forms").remove({_id: form._id}, function(err, res) {
                 if (err) throw err;
                 console.log("1 document deleted");
                 db.close();
@@ -45,7 +46,8 @@ router.post('/', function(req, res, next) {
         }
 
     });
-/*
+
+
     const fs = require('fs');
     fs.writeFile("/Users/Jacek/WebstormProjects/Data Workflow/data-workflow/src/file.json", form, function(err) {
 
@@ -56,7 +58,7 @@ router.post('/', function(req, res, next) {
         console.log("The file was saved!");
     });
 
- */
+
 
 });
 
